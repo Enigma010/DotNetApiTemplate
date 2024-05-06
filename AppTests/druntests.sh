b0a8e6ad84c3caeffd6daa0ec0dcfbec1a29b3fd
Enigma010 <jjodarsk@yahoo.com>
2024-05-05 21:07:42 -0500

    Excluding Db from test coverage

--- AppTests/druntests.sh
+++ AppTests/druntests.sh
@@ -5,7 +5,7 @@ rm -Rf /app/Output/AppTests
 cd /app/Api
 dotnet build Api.sln
 cd /app/AppTests
-dotnet test --collect:"XPlat Code Coverage" --results-directory /app/Output/AppTests/TestResults
+dotnet test --collect:"XPlat Code Coverage" --results-directory /app/Output/AppTests/TestResults --settings CodeCoverage.runsettings.xml
 guid=$(ls /app/Output/AppTests/TestResults/)
 report=$(ls /app/Output/AppTests/TestResults/$guid/coverage.cobertura.xml)
 reportgenerator -reports:$report -targetdir:"/app/Output/AppTests/TestResults/coveragereport" -reporttypes:Html
