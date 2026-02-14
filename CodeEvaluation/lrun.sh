dc1bd15d1961ab90c4bf950dc0565be0d393b78f
Enigma010 <jjodarsk@yahoo.com>
2026-02-14 08:08:21 -0600

    Base refactoring

--- CodeEvaluation/lrun.sh
+++ CodeEvaluation/lrun.sh
@@ -4,4 +4,4 @@ docker build -t apptests .
 cd ..
 volume=$(pwd)
 docker rm -f apptests 2>/dev/null || true
-docker run -it -v $volume:/app --env-file Api/.env --name apptests apptests /app/CodeEvaluation/druntests.sh
+docker run -it -v $volume:/app --env-file Solutions/App/.env --name apptests apptests /app/CodeEvaluation/druntests.sh
--- EventBus/lrun.sh
+++ EventBus/lrun.sh
@@ -1,1 +1,2 @@
+docker rm -f DotNetEventBus || true
 docker run -d --name DotNetEventBus -p 5672:5672 -p 8080:15672 rabbitmq:3-management
